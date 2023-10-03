d59790f6c321f16ed30182c5fedc611522cafd20
Marc Escandell <99950510+MrMonkey420@users.noreply.github.com>
2023-10-03 10:20:29 +0200

    PAREN LO MEMEEEEEEEEEEE

---
+++ LAS INTELIGENTES Y ARTIFICIALES AVENTURAS DE PEDRITO/Assets/Scripts/FlockManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    public GameObject prefab;
+    public int numberOfAgents;
+
+    public float minSpeed = 1.0f;
+    public float maxSpeed = 4.0f;
+    public float neighbourDistance = 1.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
