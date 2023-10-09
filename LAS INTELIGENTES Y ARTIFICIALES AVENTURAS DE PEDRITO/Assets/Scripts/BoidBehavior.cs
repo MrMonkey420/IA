29a7c31c4fdbc2bf91a8f9afb70c71e89a4d6774
Hacienda69 <fannumero1dehacienda@gmail.com>
2023-10-09 14:07:34 +0200

    pinguino

--- LAS INTELIGENTES Y ARTIFICIALES AVENTURAS DE PEDRITO/Assets/Scripts/BoidBehavior.cs
+++ LAS INTELIGENTES Y ARTIFICIALES AVENTURAS DE PEDRITO/Assets/Scripts/BoidBehavior.cs
@@ -5,54 +5,111 @@ using UnityEngine;
 
 public class BoidBehavior : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float cohesionRadius = 3.0f;
-    public float separationDistance = 1.0f;
-    public float alignmentStrength = 1.0f;
-    public float randomnessFactor = 0.2f;
+    // Flocking parameters
+    public float neighbourDistance = 3.0f;
+    public float speed = 2.0f;
+    public float rotationSpeed = 4.0f;
 
-    private Vector3 velocity; // Add the velocity field here
+    // Random behavior parameters
+    public float randomForce = 0.5f;
+    public float randomInterval = 2.0f;
+
+    // Leader behavior (optional)
+    public GameObject leader;
+    public float followDistance = 5.0f;
+    public float followForce = 1.0f;
+
+    private Vector3 direction;
+    private Vector3 cohesion;
+    private Vector3 alignment;
+    private Vector3 separation;
+    private float randomTimer;
 
     void Start()
     {
-        // Initialize the velocity
-        velocity = transform.forward * speed;
+        randomTimer = UnityEngine.Random.Range(0, randomInterval);
     }
 
     void Update()
     {
-        // Get nearby Boids within cohesionRadius
-        Collider[] nearbyBoids = Physics.OverlapSphere(transform.position, cohesionRadius);
+        Flock();
+        RandomBehavior();
+        FollowLeader();
+        Move();
+    }
 
+    void Flock()
+    {
         Vector3 cohesion = Vector3.zero;
-        Vector3 separation = Vector3.zero;
         Vector3 alignment = Vector3.zero;
+        Vector3 separation = Vector3.zero;
+        int numNeighbors = 0;
 
-        foreach (var boid in nearbyBoids)
+        foreach (GameObject otherBee in GameObject.FindGameObjectsWithTag("Bee"))
         {
-            if (boid != this) // Exclude the current Boid
+            if (otherBee != gameObject)
             {
-                Vector3 toBoid = boid.transform.position - transform.position;
+                float distance = Vector3.Distance(transform.position, otherBee.transform.position);
 
-                // Cohesion
-                cohesion += toBoid;
+                if (distance < neighbourDistance)
+                {
+                    numNeighbors++;
 
-                // Separation
-                if (toBoid.magnitude < separationDistance)
-                    separation -= toBoid.normalized / toBoid.magnitude;
+                    // Cohesion: Move towards the center of mass of neighbors.
+                    cohesion += otherBee.transform.position;
 
-                // Alignment
-                alignment += boid.GetComponent<BoidBehavior>().velocity;
+                    // Alignment: Align with the average heading of neighbors.
+                    alignment += otherBee.GetComponent<BoidBehavior>().direction;
+
+                    // Separation: Avoid crowding neighbors.
+                    separation += (transform.position - otherBee.transform.position) / (distance * distance);
+                }
             }
         }
 
-        // Apply randomness
-        Vector3 randomness = new Vector3(UnityEngine.Random.Range(-randomnessFactor, randomnessFactor), 0, UnityEngine.Random.Range(-randomnessFactor, randomnessFactor));
+        if (numNeighbors > 0)
+        {
+            cohesion /= numNeighbors;
+            alignment /= numNeighbors;
+            separation /= numNeighbors;
 
-        // Calculate new velocity
-        velocity = (cohesion + separation + alignment + randomness).normalized * speed;
+            // Calculate the final direction as a combination of the three behaviors.
+            direction = cohesion + alignment + separation;
+        }
+    }
 
-        // Update Boid's position
-        transform.position += velocity * Time.deltaTime;
+    void RandomBehavior()
+    {
+        // Add random behavior to the bee's movement.
+        randomTimer -= Time.deltaTime;
+        if (randomTimer <= 0)
+        {
+            // Generate a random force.
+            Vector3 randomForceVector = UnityEngine.Random.insideUnitSphere * randomForce;
+            direction += randomForceVector.normalized;
+            randomTimer = UnityEngine.Random.Range(0, randomInterval);
+        }
+    }
+
+    private void FollowLeader()
+    {
+        if (leader != null)
+        {
+            float distanceToLeader = Vector3.Distance(transform.position, leader.transform.position);
+
+            if (distanceToLeader > followDistance)
+            {
+                // Calculate force to follow the leader.
+                Vector3 followForceVector = (leader.transform.position - transform.position).normalized * followForce;
+                direction += followForceVector;
+            }
+        }
+    }
+
+    private void Move()
+    {
+        direction.Normalize();
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime);
+        transform.Translate(0.0f, 0.0f, Time.deltaTime * speed);
     }
 }
