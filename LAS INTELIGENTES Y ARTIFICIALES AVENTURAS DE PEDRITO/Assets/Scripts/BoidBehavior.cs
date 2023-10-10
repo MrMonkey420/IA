72937a3443ee177c49567f122472eb66c1217729
Marc Escandell <marc.escandell.alonso@estudiantat.upc.edu>
2023-10-10 12:33:24 +0200

    Me pinchan y no sangro

--- LAS INTELIGENTES Y ARTIFICIALES AVENTURAS DE PEDRITO/Assets/Scripts/BoidBehavior.cs
+++ LAS INTELIGENTES Y ARTIFICIALES AVENTURAS DE PEDRITO/Assets/Scripts/BoidBehavior.cs
@@ -5,111 +5,93 @@ using UnityEngine;
 
 public class BoidBehavior : MonoBehaviour
 {
-    // Flocking parameters
-    public float neighbourDistance = 3.0f;
-    public float speed = 2.0f;
-    public float rotationSpeed = 4.0f;
+    public float BoundRadius = 5f;
+    public FlockManager myManager;
+    private SphereCollider bounds;
 
-    // Random behavior parameters
-    public float randomForce = 0.5f;
-    public float randomInterval = 2.0f;
+    [SerializeField] private float speed;
+    [SerializeField] private Vector3 CohesionPoint;
+    [SerializeField] private Vector3 SeparationPoint;
+    [SerializeField] private Vector3 AlignmentPoint;
+    [SerializeField] private Vector3 Direction = Vector3.up;
 
-    // Leader behavior (optional)
-    public GameObject leader;
-    public float followDistance = 5.0f;
-    public float followForce = 1.0f;
-
-    private Vector3 direction;
-    private Vector3 cohesion;
-    private Vector3 alignment;
-    private Vector3 separation;
-    private float randomTimer;
-
-    void Start()
-    {
-        randomTimer = UnityEngine.Random.Range(0, randomInterval);
-    }
-
-    void Update()
-    {
-        Flock();
-        RandomBehavior();
-        FollowLeader();
-        Move();
-    }
-
-    void Flock()
+    //-----------------------------------------------------------
+    private void Cohesion()
     {
         Vector3 cohesion = Vector3.zero;
-        Vector3 alignment = Vector3.zero;
-        Vector3 separation = Vector3.zero;
-        int numNeighbors = 0;
-
-        foreach (GameObject otherBee in GameObject.FindGameObjectsWithTag("Bee"))
+        int num = 0;
+        foreach (GameObject go in myManager.Fishes)
         {
-            if (otherBee != gameObject)
+            if (go != this.gameObject)
             {
-                float distance = Vector3.Distance(transform.position, otherBee.transform.position);
-
-                if (distance < neighbourDistance)
+                float distance = Vector3.Distance(go.transform.position, transform.position);
+                if (distance <= myManager.neighbourDistance)
                 {
-                    numNeighbors++;
-
-                    // Cohesion: Move towards the center of mass of neighbors.
-                    cohesion += otherBee.transform.position;
-
-                    // Alignment: Align with the average heading of neighbors.
-                    alignment += otherBee.GetComponent<BoidBehavior>().direction;
-
-                    // Separation: Avoid crowding neighbors.
-                    separation += (transform.position - otherBee.transform.position) / (distance * distance);
+                    cohesion += go.transform.position;
+                    num++;
                 }
             }
         }
+        if (num > 0)
+            cohesion = (cohesion / num - transform.position).normalized * speed;
 
-        if (numNeighbors > 0)
-        {
-            cohesion /= numNeighbors;
-            alignment /= numNeighbors;
-            separation /= numNeighbors;
-
-            // Calculate the final direction as a combination of the three behaviors.
-            direction = cohesion + alignment + separation;
-        }
+        CohesionPoint = cohesion;
     }
-
-    void RandomBehavior()
+    private void Separation()
     {
-        // Add random behavior to the bee's movement.
-        randomTimer -= Time.deltaTime;
-        if (randomTimer <= 0)
+        Vector3 separation = Vector3.zero;
+        foreach (GameObject go in myManager.Fishes)
         {
-            // Generate a random force.
-            Vector3 randomForceVector = UnityEngine.Random.insideUnitSphere * randomForce;
-            direction += randomForceVector.normalized;
-            randomTimer = UnityEngine.Random.Range(0, randomInterval);
-        }
-    }
-
-    private void FollowLeader()
-    {
-        if (leader != null)
-        {
-            float distanceToLeader = Vector3.Distance(transform.position, leader.transform.position);
-
-            if (distanceToLeader > followDistance)
+            if (go != this.gameObject)
             {
-                // Calculate force to follow the leader.
-                Vector3 followForceVector = (leader.transform.position - transform.position).normalized * followForce;
-                direction += followForceVector;
+                float distance = Vector3.Distance(go.transform.position, transform.position);
+                if (distance <= myManager.neighbourDistance)
+                    separation -= (transform.position - go.transform.position) / (distance * distance);
             }
         }
+
+        SeparationPoint = separation;
+    }
+    private void Alignment()
+    {
+        Vector3 align = Vector3.zero;
+        int num = 0;
+        foreach (GameObject go in myManager.Fishes)
+        {
+            if (go != this.gameObject)
+            {
+                float distance = Vector3.Distance(go.transform.position, transform.position);
+                if (distance <= myManager.neighbourDistance)
+                {
+                    align += go.GetComponent<BoidBehavior>().Direction;
+                    num++;
+                }
+            }
+        }
+        if (num > 0)
+        {
+            align /= num;
+            speed = Mathf.Clamp(align.magnitude, myManager.minSpeed, myManager.maxSpeed);
+        }
+
+        AlignmentPoint = align;
     }
 
-    private void Move()
+    //------------------------------------------------------------
+    void Start()
     {
-        direction.Normalize();
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime);
+        bounds = GetComponent<SphereCollider>();
+        bounds.radius = BoundRadius;
+    }
+    void Update()
+    {
+        Cohesion();
+        Separation();
+        Alignment();
+
+        Direction = (CohesionPoint + AlignmentPoint + SeparationPoint).normalized * speed;
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Direction), myManager.rotationSpeed * Time.deltaTime);
         transform.Translate(0.0f, 0.0f, Time.deltaTime * speed);
     }
 }
--- LAS INTELIGENTES Y ARTIFICIALES AVENTURAS DE PEDRITO/Assets/Scripts/FlockManager.cs
+++ LAS INTELIGENTES Y ARTIFICIALES AVENTURAS DE PEDRITO/Assets/Scripts/FlockManager.cs
@@ -5,21 +5,52 @@ using UnityEngine;
 
 public class FlockManager : MonoBehaviour
 {
-    public GameObject agentPrefab;
-    public int numberOfAgents = 10;
-    public float spawnRadius = 2.0f;
+    public GameObject fishPrefab;
+    public int numFishes;
 
-    void Start()
+    public float maxSpeed = 5.0f;
+    public float minSpeed = 1.0f;
+
+    public float neighbourDistance = 2.0f;
+    public float rotationSpeed = 2.0f;
+    public bool Randomize = false;
+    [SerializeField] private Vector3 center;
+
+    public GameObject[] Fishes;
+    
+    //------------------------------------------------------------
+    private void CalculateCenter()
     {
-        SpawnAgents();
+        Vector3 positions = new Vector3();
+
+        for (int i = 0; i < numFishes; i++)
+        {
+            positions += Fishes[i].transform.position;
+        }
+
+        center = new Vector3(positions.x / numFishes, positions.y / numFishes, positions.z / numFishes);
     }
 
-    void SpawnAgents()
+    //------------------------------------------------------------
+    void Start()
     {
-        for (int i = 0; i < numberOfAgents; i++)
+        Fishes = new GameObject[numFishes];
+
+        for (int i = 0; i < numFishes; ++i)
         {
-            Vector3 spawnPosition = transform.position + UnityEngine.Random.insideUnitSphere * spawnRadius;
-            GameObject agent = Instantiate(agentPrefab, spawnPosition, Quaternion.identity);
+            Vector3 pos = this.transform.position + UnityEngine.Random.insideUnitSphere * 10; // Random position
+            Vector3 randomize = UnityEngine.Random.insideUnitSphere; // Random vector direction
+
+            // Ensure that the fish are at a consistent depth (e.g., swimming near the surface)
+            pos.y = Mathf.Clamp(pos.y, 1.0f, 10.0f);
+
+            Fishes[i] = Instantiate(fishPrefab, pos, Quaternion.LookRotation(randomize));
+            Fishes[i].GetComponent<BoidBehavior>().myManager = this;
         }
+    }
+    void Update()
+    {
+        CalculateCenter();
+        transform.position = center;
     }
 }
