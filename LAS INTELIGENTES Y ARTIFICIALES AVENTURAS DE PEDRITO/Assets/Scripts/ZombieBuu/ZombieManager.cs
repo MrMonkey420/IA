404458809348e1260b0f16e282d28a781d93a986
Marc Escandell <99950510+MrMonkey420@users.noreply.github.com>
2023-10-17 08:47:33 +0200

    buuuuuuu

---
+++ LAS INTELIGENTES Y ARTIFICIALES AVENTURAS DE PEDRITO/Assets/Scripts/ZombieBuu/ZombieManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieManager : MonoBehaviour
+{
+    public GameObject ZombiePrefab;
+    public GameObject Zombies;
+    public GameObject Spawners;
+
+    public float velocity;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
